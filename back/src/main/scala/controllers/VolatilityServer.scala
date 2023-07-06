ae3e0a7f020466f41e54acdf44303c49e99e440d
Tiago Cavalcanti <tiagodeholandacavalcanti@gmail.com>
2023-07-06 12:43:58 -0300

    use Executors api to repeatedly call binance

--- back/src/main/scala/controllers/VolatilityServer.scala
+++ back/src/main/scala/controllers/VolatilityServer.scala
@@ -14,26 +14,30 @@ import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
 import org.slf4j.{Logger, LoggerFactory}
 
+import java.util.concurrent.{Executors, TimeUnit}
 import scala.collection.concurrent.TrieMap
 
 object VolatilityServer extends IOApp {
   val cache: TrieMap[String, UnderlyingAsset] = TrieMap()
 
   val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "data" => Ok(cache.keySet)
+    case GET -> Root / "data" => Ok(cache.keySet.toList.sorted)
     case GET -> Root / "data" / asset => Ok(cache(asset))
   }
 
-  new Thread(() => while (true) {
+  def updateCache(): Unit = {
     ExchangeClient.fetchMarketPrices
       .map(_.map(asset => asset.copy(bestSurface = calibrate(asset)))
         .foreach(asset => cache.put(asset.symbol, asset)))(ExchangeClient.executionContext)
-    Thread.sleep(10000)
-  }).start()
+  }
 
   LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext].getLogger(Logger.ROOT_LOGGER_NAME).setLevel(Level.OFF)
 
   def run(args: List[String]): IO[ExitCode] = {
+    Executors
+      .newSingleThreadScheduledExecutor()
+      .scheduleAtFixedRate(() => updateCache(), 0, 10, TimeUnit.SECONDS)
+
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
