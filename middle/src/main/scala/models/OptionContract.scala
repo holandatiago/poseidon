729a733226d94bebd2020d8c764d56980fda87dc
Tiago Cavalcanti <tiagodeholandacavalcanti@gmail.com>
2023-07-07 00:49:44 -0300

    remove package object models

---
+++ middle/src/main/scala/models/OptionContract.scala
@@ -0,0 +1,19 @@
+package models
+
+case class OptionContract(
+    symbol: String = null,
+    underlying: String = null,
+    termTimestamp: Long = Long.MinValue,
+    strike: Double = Double.NaN,
+    side: String = null,
+    volatility: Double = Double.NaN,
+    spread: Double = Double.NaN,
+    timeToExpiry: Double = Double.NaN,
+    logMoneyness: Double = Double.NaN) {
+
+  def withAssetPrice(asset: UnderlyingAsset): OptionContract = copy(
+    timeToExpiry = (termTimestamp - asset.currentTimestamp) / (365 * 24 * 60 * 60 * 1000D),
+    logMoneyness = math.log(strike) - math.log(asset.spot))
+
+  def isValid: Boolean = volatility > 0.01 && volatility < 10 - 0.01
+}
---
+++ middle/src/main/scala/models/UnderlyingAsset.scala
@@ -0,0 +1,15 @@
+package models
+
+case class UnderlyingAsset(
+    symbol: String = null,
+    currencyPair: (String, String) = null,
+    currentTimestamp: Long = Long.MinValue,
+    spot: Double = Double.NaN,
+    options: List[OptionContract] = null,
+    bestSurface: VolatilitySurface = null) {
+
+  def withOptionList(optionList: List[OptionContract]): UnderlyingAsset = copy(
+    options = optionList.filter(_.isValid).sortBy(_.symbol).map(_.withAssetPrice(this)))
+
+  def isValid: Boolean = options.nonEmpty
+}
