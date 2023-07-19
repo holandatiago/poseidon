a850742324635a60cee6467af48e5e96a2b6a1b3
Tiago Cavalcanti <tiagodeholandacavalcanti@gmail.com>
2023-07-19 10:47:47 -0300

    add footer

--- front/src/main/scala/page/Client.scala
+++ front/src/main/scala/page/Client.scala
@@ -65,14 +65,19 @@ object Client {
 
   def plotCanvas(): HtmlElement = {
     div(cls := "col-md-9 ms-sm-auto col-lg-10 px-md-4",
-      div(cls := "d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom",
+      div(cls := "d-flex justify-content-between flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom",
         h1(cls := "h2", "Volatility Smile"),
         div(cls := "d-flex align-items-center gap-2",
           label(forId := "expiryDate", "Expiry:"),
           select(cls := "form-select", idAttr := "expiryDate",
             children <-- termsSignal.map(_.map(createSelectOption)),
             onChange.mapToValue.map(Some(_).map(_.toLong)) --> termVar))),
-      Plot.renderDataChart())
+      Plot.renderDataChart(),
+      div(cls := "d-flex justify-content-between flex-md-nowrap align-items-center",
+        a(cls := "text-muted", "Data provided by Binance", target := "_blank",
+          href <-- optionsSignal.map(_.map(createDataProvidedText).getOrElse(""))),
+        a(cls := "text-muted link-underline link-underline-opacity-0",
+          child.text <-- assetVar.signal.map(_.map(createLastUpdatedText).getOrElse("")))))
   }
 
   def createSelectOption(ts: Long): HtmlElement = {
@@ -82,5 +87,13 @@ object Client {
   def fetchData[T: Decoder](path: String = null): Signal[Option[T]] = {
     AjaxStream.get(List("data").appendedAll(Option(path)).mkString("/"))
       .map(_.responseText).map(decode[T](_).toOption).toSignal(None)
+  }
+
+  def createDataProvidedText(pair: (UnderlyingAsset, Long)): String = pair match { case (asset, term) =>
+    s"https://www.binance.com/en/eoptions/${asset.symbol}?expirationDate=${new js.Date(term).toISOString.take(10)}"
+  }
+
+  def createLastUpdatedText(asset: UnderlyingAsset): String = {
+    s"Last updated at ${new js.Date(asset.currentTimestamp).toISOString}"
   }
 }
